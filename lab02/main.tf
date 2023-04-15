14b1fe33ca374ff09e2820aec1fa7123d26e69f6
ccruzado <ccruzado@outlook.com>
2023-04-15 13:19:45 -0500

    change eks

--- lab02/main.tf
+++ lab02/main.tf
@@ -5,19 +5,6 @@ provider "aws" {
   region  = var.aws_region
 }
 
-provider kubernetes {
-  host                   = element(concat(data.aws_eks_cluster.cluster[*].endpoint, [""]), 0)
-  cluster_ca_certificate = base64decode(element(concat(data.aws_eks_cluster.cluster[*].certificate_authority.0.data, [""]), 0))
-  token                  = element(concat(data.aws_eks_cluster_auth.cluster[*].token, [""]), 0)
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = element(concat(data.aws_eks_cluster.cluster[*].endpoint, [""]), 0)
-    token                  = element(concat(data.aws_eks_cluster_auth.cluster[*].token, [""]), 0)
-    cluster_ca_certificate = base64decode(element(concat(data.aws_eks_cluster.cluster[*].certificate_authority.0.data, [""]), 0))
-  }
-}
 #############################################################################
 # ETIQUETAS
 #############################################################################
